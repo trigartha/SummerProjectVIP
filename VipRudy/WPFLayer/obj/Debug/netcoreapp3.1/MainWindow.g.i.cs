8386ad6cef517ea17a9adf629244e779e07efe0a
trigartha <57434025+trigartha@users.noreply.github.com>
2020-08-07 20:08:36 +0200

    adapted addreservationview

--- VipRudy/WPFLayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ VipRudy/WPFLayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace WPFLayer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFLayer;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFLayer;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
